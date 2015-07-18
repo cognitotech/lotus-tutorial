da61e60fb6b9ed6a0e43f3e31d1fa54d399ad6c6
Scott Le <scott.lee318@gmail.com>
2015-07-18 14:15:08 +0700

    included handler

--- lib/authentications/web_strategy.rb
+++ lib/authentications/web_strategy.rb
@@ -2,6 +2,13 @@ module Authentication
   UnauthenticatedError = Class.new(StandardError)
 
   module WebStrategy
+    def self.included(klass)
+      klass.class_eval do
+        handle_exception UnauthenticatedError => :redirect_to_login
+        before :authenticate!
+      end
+    end
+
     def authenticate!
       has_user_session? or raise UnauthenticatedError.new
     end
@@ -10,6 +17,10 @@ module Authentication
 
     def has_user_session?
       !!session[:user_id]
+    end
+
+    def redirect_to_login
+      redirect_to '/'
     end
   end
 end
