251f45250b2f0f63c7ebde15c785215d3ce0ab84
Scott Le <scott.lee318@gmail.com>
2015-07-25 19:11:09 +0700

    check login in user repository

--- spec/lotus-tutorial/repositories/user_repository_spec.rb
+++ spec/lotus-tutorial/repositories/user_repository_spec.rb
@@ -2,4 +2,23 @@ require 'spec_helper'
 
 RSpec.describe UserRepository do
   # place your tests here
+  describe "#check_login" do
+    let(:email) { 'quangkhai@ssf.vn' }
+    let(:password) { '123' }
+    before do
+      @user = User.new(email: 'quangkhai@ssf.vn', password: '123', name: 'Khai Le')
+      @user = UserRepository.create(@user)
+    end
+    context 'valid credentials' do
+      it 'returns the user' do
+        expect(UserRepository.check_login(email, password)).to eq @user
+      end
+    end
+
+    context 'invalid credentials' do
+      it 'returns nil' do
+        expect(UserRepository.check_login(email, 'abc')).to be_nil
+      end
+    end
+  end
 end
