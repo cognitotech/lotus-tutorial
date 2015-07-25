ca22b7a10e79ad8f89a1df2231cfe3059bc0b5b4
Scott Le <scott.lee318@gmail.com>
2015-07-25 15:54:39 +0700

    minor fix

--- spec/web/views/registration/create_spec.rb
+++ spec/web/views/registration/create_spec.rb
@@ -3,7 +3,7 @@ require_relative '../../../../apps/web/views/registration/create'
 
 describe Web::Views::Registration::Create do
   let(:exposures) { Hash[foo: 'bar'] }
-  let(:template)  { Lotus::View::Template.new('apps/web/templates/registration/create.html.erb') }
+  let(:template)  { Lotus::View::Template.new('apps/web/templates/registration/index.html.erb') }
   let(:view)      { Web::Views::Registration::Create.new(template, exposures) }
   let(:rendered)  { view.render }
 
